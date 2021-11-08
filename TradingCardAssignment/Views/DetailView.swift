1f7cde4135b14aadf621fa1945908105da6fa27f
Luke Newbigging <lnewbigging@lcs.on.ca>
2021-11-08 13:24:30 -0500

    Have set of a basic Detail view with imported photos of Fred VanVleet and OG.

--- TradingCardAssignment/Views/DetailView.swift
+++ TradingCardAssignment/Views/DetailView.swift
@@ -8,13 +8,46 @@
 import SwiftUI
 
 struct DetailView: View {
+    let item: TradingCard
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        
+        ScrollView {
+            VStack(alignment:.leading){
+                
+                PhotoCaptionView(imageName: "Fred VanVleet",
+                                 caption: "Fred VanVleet is the starting PG on the Toronto Raptors",
+                                 PPG: "PPG: 12.1",
+                                 APG: "APG: 4,5",
+                                 FGP: "FG%: .405%",
+                                 MPG: "MPG: 26.2",
+                                 FFT: "FFT% .855%")
+                    
+                    
+                
+                
+                
+                Text(item.bodyText)
+                
+                
+                
+                    .padding()
+                
+                Spacer()
+            }
+        }
+        .navigationTitle("Fred VanVleet")
     }
 }
 
+
 struct DetailView_Previews: PreviewProvider {
     static var previews: some View {
-        DetailView()
+        NavigationView {
+            DetailView(item:listOfStats.first!)
+        }
     }
 }
+
+
+
--- TradingCardAssignment/Views/Shared/EnhancedListItemView.swift
+++ TradingCardAssignment/Views/Shared/EnhancedListItemView.swift
@@ -8,13 +8,41 @@
 import SwiftUI
 
 struct EnhancedListItemView: View {
+    
+    // MARK: Stored properties
+    let imageName: String
+    let title: String
+    let subtitle: String
+    
+    // MARK: Computed properties
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            
+            Image(imageName)
+                .resizable()
+                .scaledToFill()
+                .frame(width: 50, height: 50, alignment: .center)
+                .clipped()
+            
+            VStack(alignment: .leading) {
+                
+                Text(title)
+                    .bold()
+                
+                Text(subtitle)
+                    .font(.caption)
+                
+            }
+            
+        }
     }
 }
 
 struct EnhancedListItemView_Previews: PreviewProvider {
     static var previews: some View {
-        EnhancedListItemView()
+        EnhancedListItemView(imageName: "Fred VanVleet",
+                             title: "Fred VanVleet",
+                             subtitle: "Fred VanVleet is the starting PG for the Toronto Raptors.")
     }
 }
+
